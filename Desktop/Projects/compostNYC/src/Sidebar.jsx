5de65640d2246e5624814f38d3002631bbf793af
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-21 19:39:46 -0400

    map 100% view; 5 locations added

---
+++ Desktop/Projects/compostNYC/src/Sidebar.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+// export default function SideBar() {
+//   return (
+//     <>
+//     <div id="sidebar">SideBar!!!</div>
+//     <p>HI</p>
+//     </>
+//   )
+// }
